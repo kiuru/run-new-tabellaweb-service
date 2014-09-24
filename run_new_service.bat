69cb9ec288e6eca56bfb23be0f108501baa8f334
Niko Kiuru <niko@kiuru.net>
2014-09-24 13:57:20 +0300

    Only one location were have to define service's name

--- run_new_service.bat
+++ run_new_service.bat
@@ -1,16 +1,6 @@
 @echo off
-echo #################################################################################
-echo ##                                                                             ##
-echo ##  Create new TabellaWEB service automatically                                ##
-echo ##  Location: HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TabellaWEB  ##
-echo ##  Author: Niko Kiuru                                                         ##
-echo ##  http://NikoKiuru.com                                                       ##
-echo ##  Date: 2014-05-16                                                           ##
-echo ##                                                                             ##
-echo #################################################################################
-echo.
-
-PAUSE
+REM # Define service name
+set ServiceName=TabellaWEB
 
 :: BatchGotAdmin
 :-------------------------------------
@@ -19,39 +9,21 @@ REM  --> Check for permissions
 
 REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
+	echo Permission denied! Administrators privileges required. %ServiceName%
+	PAUSE
+	exit /B
+)
 :--------------------------------------
 REM ## Administrator permission request source: http://stackoverflow.com/a/10052222
 
 REM ## Create new service to register
-
-sc delete TabellaWEB
-sc create TabellaWEB binPath= %CD%\srvany.exe start= auto
-sc description TabellaWEB "Tabella WEB-palvelinohjelma"
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\TabellaWEB\Parameters" /v "Application" /d "%CD%\TABELLA.exe" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\TabellaWEB\Parameters" /v "AppDirectory" /d "%CD%" /f
-reg add "HKLM\SYSTEM\CurrentControlSet\Services\TabellaWEB\Parameters" /v "AppParameters" /d "/autostart" /f
+sc delete %ServiceName%
+sc create %ServiceName% binPath= %CD%\srvany.exe start= auto
+sc description %ServiceName% "%ServiceName% -palvelinohjelma"
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceName%\Parameters" /v "Application" /d "%CD%\TABELLA.exe" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceName%\Parameters" /v "AppDirectory" /d "%CD%" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\%ServiceName%\Parameters" /v "AppParameters" /d "/autostart" /f
 
 echo.
-echo #################################################
-echo ##                                             ##
-echo ##  New TabellaWEB service added succesfully!  ##
-echo ##                                             ##
-echo #################################################
-
+echo New Tabella WEB service added succesfully!
 PAUSE
